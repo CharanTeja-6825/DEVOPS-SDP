e63fbee7cd31d7b9c358e2c16ba3c0e0d4c73db9
CharanTeja <rcharanteja2006@gmail.com>
2025-09-22 21:14:52 +0530

    Configured for Docker

--- BudgetPlanner-Backend/BudgetPlanner/src/main/java/com/BudgetPlanner/sdp/modal/Income.java
+++ BudgetPlanner-Backend/BudgetPlanner/src/main/java/com/BudgetPlanner/sdp/modal/Income.java
@@ -1,14 +1,11 @@
 package com.BudgetPlanner.sdp.modal;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
